6e740a08c0062205acc875fc5fdc7d4889d1d01d
moon472907 <moon472907@gmail.com>
2025-05-29 14:50:06 +0900

    aa

---
+++ src/test/java/com/back/AppTest.java
@@ -0,0 +1,14 @@
+package com.back;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AppTest
+{
+    @Test
+    @DisplayName("테스트 1")
+    void t1(){
+        // 테스트 로직 작성
+        System.out.println("테스트 1 실행");
+    }
+}
